e910119c0bbbec70bee4752b86fed47da26d1dc6
Linda Chang <lindachang186@gmail.com>
2018-06-25 15:38:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,13 +15,3 @@ function whileLoop(n){
   }
   return ("done")
 }
-
-function doWhileLoop(array){
-  function maybeTrue{
-  return Math.random() >= 0.5
-}
-do {
-  console.log('done')
-} while (maybeTrue())
-return array
-}
